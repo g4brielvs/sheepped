1585f8fbf9e52f1b62e94cc172fd307afdf969a5
Eduardo Cuducos <cuducos@users.noreply.github.com>
2019-10-13 17:09:51 -0400

    Isolates URL generation

--- sheepped/__init__.py
+++ sheepped/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import json
+
 import requests
 import xmltodict
-
 from lxml import etree
 
 
@@ -12,25 +12,26 @@ class USPSError(Exception):
 
 
 class USPS:
-    '''
+    """
     Wrapper around USPS tracking API
-
     :param str user_id: USPS User ID
-    '''
+    """
+
+    URL = "https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML="
+
     def __init__(self, user_id):
         self.user_id = user_id
 
-    API_URL = 'https://secure.shippingapis.com/ShippingAPI.dll?API='
+    def url_for(self, tracking_number):
+        xml = etree.Element("TrackFieldRequest", {"USERID": self.user_id})
+        etree.SubElement(xml, "TrackID", {"ID": tracking_number})
+        value = etree.tostring(xml).decode()
+        return f"{self.URL}{value}"
 
     def track(self, tracking_number):
-        xml = etree.Element('TrackFieldRequest', {'USERID': self.user_id})
-        child = etree.SubElement(xml, 'TrackID', {'ID': tracking_number})
-
-        xml = etree.tostring(xml).decode()
-        url = f'{self.API_URL}TrackV2&XML={xml}'
-
+        url = self.url_for(tracking_number)
         xml_response = requests.get(url).content
         response = json.loads(json.dumps(xmltodict.parse(xml_response)))
-        if 'Error' in response:
-            raise USPSError(response['Error'])
+        if "Error" in response:
+            raise USPSError(response["Error"])
         return response
--- tests.py
+++ tests.py
@@ -5,16 +5,26 @@ from sheepped import USPS, USPSError
 
 
 class TestUSPS(TestCase):
+    EXPECTED_URL = (
+        "https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML="
+        '<TrackFieldRequest USERID="test-id">'
+        '<TrackID ID="42"/>'
+        "</TrackFieldRequest>"
+    )
+
     def setUp(self):
         self.usps = USPS("test-id")
 
     @patch("sheepped.requests.get")
     def test_successful_track(self, get):
         get.return_value.content = "<Response>Ok</Response>"
-        self.assertEqual(self.usps.track("42"), {"Response": "Ok"})
+        response = self.usps.track("42")
+        self.assertEqual(response, {"Response": "Ok"})
+        get.assert_called_once_with(self.EXPECTED_URL)
 
     @patch("sheepped.requests.get")
     def test_failed_track(self, get):
         get.return_value.content = "<Error>Oops</Error>"
         with self.assertRaisesRegexp(USPSError, "Oops"):
             self.usps.track("42")
+        get.assert_called_once_with(self.EXPECTED_URL)
